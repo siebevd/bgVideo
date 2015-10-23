0c0fb3bf9517ad9759318fe49896680f4d4f4e10
Siebe <vandijckske@gmail.com>
2015-10-24 00:51:43 +0100

    small updates

--- jquery.bgvideo.js
+++ jquery.bgvideo.js
@@ -15,27 +15,28 @@
         }, options);
 
         var self = this;
-        function loadHandler(){
-          if(Modernizr.video){
-            if(!settings.mobileVideo){
-              setVideoProperties();
-            }else{
-              if($(window).width() > settings.mobileVideo){
-                setVideoProperties();
-              }else{
-                setImageProperties();
-              }
-            }
-          }else{
-            setImageProperties();
-          }
 
-          resizeHandler(null);
+        function loadHandler(){
+            if(Modernizr.video){
+                if(!settings.mobileVideo){
+                    setVideoProperties();
+                }else{
+                    if($(window).width() > settings.mobileVideo){
+                        setVideoProperties();
+                    }else{
+                        setImageProperties();
+                    }
+                }
+            }else{
+                setImageProperties();
+            }
+
+            resizeHandler(null);
         }
         function setVideoProperties(){
             $(self).append("<video "+ settings.options+" class='bgVideoSrc'></video>");
             if(settings.mp4Src){
-              $(self).find('video').append("<source src='" + settings.mp4Src + "'  type='video/mp4'></source>")
+                $(self).find('video').append("<source src='" + settings.mp4Src + "'  type='video/mp4'></source>")
             }
             if(settings.ogvSrc){
                 $(self).find('video').append("<source src='" + settings.ogvSrc + "'  type='video/ogg codecs=theora,vorbis'></source>")
@@ -49,54 +50,53 @@
 
         function resizeHandler(e){
 
-              if(Modernizr.video){
-              var width = $(self).width();
-              var height = $(self).height();
-              $(self).css({'overflow':'hidden'});
-              $(self).find('.bgVideoSrc').css({'position':'absolute'});
+            if(Modernizr.video){
+                var width = $(self).width(),
+                    height = $(self).height(),
+                    imgEq = width/height,
+                    videoEq = settings.originalWidth/settings.originalHeight,
+                    vw = width,
+                    vh = width/videoEq;
 
-              var imgEq = width/height;
-              var videoEq = settings.originalWidth/settings.originalHeight;
-              var vw = width;
-              var vh = width/videoEq;
+                $(self).css({'overflow':'hidden'});
+                $(self).find('.bgVideoSrc').css({'position':'absolute'});
 
-              vh = width/videoEq;
 
-              if(settings.fill){
+                vh = width/videoEq;
 
-                if(imgEq <= videoEq){
-                  vw = height * videoEq;
-                  vh = height;
-                  vDiff = (width - vw)/2;
-                  $(self).find('.bgVideoSrc').css({'left':vDiff,'top':0});
+                if(settings.fill){
 
+                    if(imgEq <= videoEq){
+                        vw = height * videoEq;
+                        vh = height;
+                        vDiff = (width - vw)/2;
+                        $(self).find('.bgVideoSrc').css({'left':vDiff,'top':0});
+
+                    }else{
+                        vw = width;
+                        hDiff = (height - vh)/2;
+                        $(self).find('.bgVideoSrc').css({'top':hDiff,'left':0});
+
+                    }
+                    $(self).find('.bgVideoSrc').css({'width':vw,"height":vh});
                 }else{
-                  vw = width;
-                  hDiff = (height - vh)/2;
-                  $(self).find('.bgVideoSrc').css({'top':hDiff,'left':0});
+                    if(imgEq <= videoEq){
+                        var centerHor = ($(self).height() - vh)/2;
+                        $(self).find('.bgVideoSrc').css({'width':vw,"height":vh,'top':centerHor,'left':0});
+                    }else{
+                        vw = height * videoEq;
+                        vh = height;
+                        var centerVer = ($(self).width() - vw)/2;
+                        $(self).find('.bgVideoSrc').css({'width':vw,"height":vh,'left':centerVer,'top':'0px'});
+                    }
 
                 }
-                 $(self).find('.bgVideoSrc').css({'width':vw,"height":vh});
-              }else{
-                if(imgEq <= videoEq){
-                  var centerHor = ($(self).height() - vh)/2;
-                  $(self).find('.bgVideoSrc').css({'width':vw,"height":vh,'top':centerHor,'left':0});
-                }else{
-                  vw = height * videoEq;
-                  vh = height;
-                  var centerVer = ($(self).width() - vw)/2;
-                  $(self).find('.bgVideoSrc').css({'width':vw,"height":vh,'left':centerVer,'top':'0px'});
-                }
-
-              }
 
             }
-          }
-          
-
+        }
 
         if(settings.resize){
-           $(window).resize(resizeHandler);
+            $(window).resize(resizeHandler);
         }
         loadHandler();
     }
